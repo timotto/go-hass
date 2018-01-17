3e2e70484875fe087d6f357484c3c5e5d081c987
Patrik Wallström <patrik@brickchain.com>
2018-01-17 13:59:18 +0100

    return err in GetDevice, also added SupportedDevices() and IsSupportedDevice()

--- device.go
+++ device.go
@@ -1,6 +1,9 @@
 package hass
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
 
 // Device is a generic interface for interacting with devices
 type Device interface {
@@ -12,15 +15,31 @@ type Device interface {
 }
 
 // GetDevice returns a Device object from an State object
-func (a *Access) GetDevice(state State) Device {
+func (a *Access) GetDevice(state State) (Device, error) {
 	dom := strings.TrimSuffix(strings.SplitAfter(state.EntityID, ".")[0], ".")
 	switch dom {
 	case "light":
-		return a.NewLight(state.EntityID)
+		return a.NewLight(state.EntityID), nil
 	case "switch":
-		return a.NewSwitch(state.EntityID)
+		return a.NewSwitch(state.EntityID), nil
 	case "lock":
-		return a.NewLock(state.EntityID)
+		return a.NewLock(state.EntityID), nil
 	}
-	return nil
+	return nil, errors.New("Device type not supported yet")
+}
+
+// SupportedDeviceTypes returns a list of supported device types
+func (a *Access) SupportedDeviceTypes() []string {
+	return []string{"light", "switch", "lock"}
+}
+
+// IsSupportedDevice returns true if an entityID is a supported device
+func (a *Access) IsSupportedDevice(id string) bool {
+	dom := strings.TrimSuffix(strings.SplitAfter(id, ".")[0], ".")
+	for _, d := range a.SupportedDeviceTypes() {
+		if dom == d {
+			return true
+		}
+	}
+	return false
 }
